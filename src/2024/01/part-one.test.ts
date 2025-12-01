cac3757667977191c97ba1ac2af53cc4a46de467
phibkro <71797726+phibkro@users.noreply.github.com>
2025-12-01 21:50:05 +0100

    group tests

--- src/2024/01/part-one.test.ts
+++ src/2024/01/part-one.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, it } from "@effect/vitest"
-import { Effect, Equal, pipe } from "effect";
+import { Effect, Equal } from "effect";
 import { transform, reduce } from "./part-one.js"
 
 describe("Part one", () => {
-  const sample = `
+  describe("sample", () => {
+
+    const sample = `
   3   4
   4   3
   2   5
@@ -12,24 +14,25 @@ describe("Part one", () => {
   3   3
   `;
 
-  const answer = 11;
+    const answer = 11;
 
-  it("should transform", () => {
-    expect(Equal.equals(
-      transform(sample),
-      [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]]
-    ));
-  });
+    it("should transform", () => {
+      expect(Equal.equals(
+        transform(sample),
+        [[3, 4], [4, 3], [2, 5], [1, 3], [3, 9], [3, 3]]
+      ));
+    });
 
-  it("should reduce", () => {
-    expect(
-      Equal.equals(
-        pipe(
-          transform(sample),
-          Effect.andThen(reduce)
-        ),
-        answer
+    it("should reduce", () => {
+      expect(
+        Equal.equals(
+          Effect.gen(function* () {
+            const s = yield* transform(sample);
+            return reduce(s);
+          }),
+          answer
+        )
       )
-    )
+    })
   })
 })
